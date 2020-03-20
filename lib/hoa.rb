6990ca911d916dcaf45a0be3a9fa7988175af54c
Marius Phoenix <phoenixzenomarius@gmail.com>
2020-03-20 16:18:42 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,9 +8,7 @@ BASE_HOA = {
 def add_character(show, name)
   
   BASE_HOA[:chipmunks] << "Dave" 
-  BASE_HOA
   BASE_HOA[:third_earthers] << "Snarf"
-  BASE_HOA
   BASE_HOA[:jetsons] << "Astro"
   BASE_HOA
   
